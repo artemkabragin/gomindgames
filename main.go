cf38505f4de1497ec0042200f6c5c90ebec26b27
Artem Bragin <sir.temab@gmail.com>
2025-10-13 23:10:13 +0300

    register, login handlers

--- main.go
+++ main.go
@@ -1,15 +1,37 @@
 package main
 
 import (
+	"log"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
 func main() {
-	initEcho()
+	// Initialize database before starting the server
+	dsn := "host=localhost user=artembragin password=1337 dbname=auth_database port=5432 sslmode=disable"
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("can't connect to database: %v", err)
+	}
+
+	log.Println("Connected to database ", db.Table("users") != nil)
+
+	if err := db.AutoMigrate(&User{}, &RefreshToken{}); err != nil {
+		log.Fatalf("can't migrate database: %v", err)
+	}
+
+	// Start Echo after DB is ready
+	e := initEcho()
+	e.Start("localhost:8080")
 }
 
-func initEcho() {
+func initEcho() *echo.Echo {
 	e := echo.New()
 	e.Use(middleware.CORS())
 	e.Use(middleware.Logger())
@@ -18,5 +40,5 @@ func initEcho() {
 	e.POST("/auth/login", postLogin)
 	e.POST("/auth/refresh", postRefresh)
 
-	e.Start("localhost:8080")
+	return e
 }
